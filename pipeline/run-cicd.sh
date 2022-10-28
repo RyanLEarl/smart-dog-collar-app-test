580ddbb54c3c7d8f96d5cf95571c6be97889d1a6
Ryan Earl <ryanleoearl@gmail.com>
2022-10-28 13:50:00 -0700

    testing further

--- pipeline/run-cicd.sh
+++ pipeline/run-cicd.sh
@@ -8,7 +8,7 @@ type=$2
 env=$3
 
 if [[ -n "$env" ]]; then
-    echo "CICD BUIlD NAME:${name} && STAGE:${env}"
+    echo "CICD BUIlD NAME:${name}"
     aws cloudformation ${type}-stack --stack-name "${name}-${env}-cicd" --template-body file://./pipeline/template.yml \
     --parameters ParameterKey=GitHubRepo,ParameterValue="${name}" ParameterKey=GitHubBranch,ParameterValue="${env}" \
     --profile default --region us-west-2 --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
