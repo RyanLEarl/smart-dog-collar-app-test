30ca8167256fab2859fa89891b066a27227c9c3d
Ryan Earl <ryanleoearl@gmail.com>
2023-02-10 13:04:16 -0800

    Forgot to remove reference to database

--- back-end/src/functions/helpers/index.js
+++ back-end/src/functions/helpers/index.js
@@ -1,5 +1,4 @@
 const { Logger } = require('./logger.js');
-const { Database } = require('./database.js');
 const { getParams } = require('./getParams.js');
 const { dig } = require('./dig.js');
 const { initCollarStep } = require('./initCollarStep.js');
@@ -8,7 +7,6 @@ const SecretHelper = require('./secret.js')
 
 module.exports = {
   Logger,
-  Database,
   getParams,
   dig,
   initCollarStep,
