13ed6c5bbe8067d494d1f9f053c321ddc9e06f0f
Ryan Earl <ryanleoearl@gmail.com>
2023-05-18 00:02:33 -0700

    Added permission and adjusted new queue and function

--- back-end/src/functions/testArduinoProcessing.js
+++ back-end/src/functions/testArduinoProcessing.js
@@ -1,14 +1,18 @@
 const { Response } = require('./helpers');
 var AWS = require("aws-sdk");
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
-exports.handler = function(event, context) {
-  var eventText = `X-value: ${event.body.xRotationalAxis} \nY-value: ${event.body.yRotationalAxis} \nZ-value: ${event.body.zRotationalAxis}`;
-  console.log("Received events:", eventText);
-  var sns = new AWS.SNS();
-  var params = {
-      Message: eventText, 
-      Subject: "Test SNS From Lambda",
-      TopicArn: "arn:aws:sns:us-west-2:209462133621:smart-dog-collar-app-test-snsTopic:73840ec3-2a42-41c4-9fec-7c3e92891a87"
+exports.handler = async(event) => {
+  const params = {
+    MessageBody: JSON.stringify(event),
+    QueueUrl: 'https://sqs.us-west-2.amazonaws.com/209462133621/smart-dog-collar-app-test-dev-ArduinoProcessedDataQueue.fifo',
+    MessageGroupId: 'my-group-id'
   };
-  sns.publish(params, context.done);
+
+  try {
+      const data = await sqs.sendMessage(params).promise();
+      console.log(`Message sent with ID: ${data.MessageId}`);
+  } catch (err) {
+      console.error(err);
+}
 };
