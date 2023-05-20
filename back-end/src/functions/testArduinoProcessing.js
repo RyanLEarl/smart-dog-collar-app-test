2844e6da940aab87d1474b26aa38368602f3ebee
Ryan Earl <ryanleoearl@gmail.com>
2023-05-19 18:36:12 -0700

    Added new messageGroupId generation

--- back-end/src/functions/testArduinoProcessing.js
+++ back-end/src/functions/testArduinoProcessing.js
@@ -1,12 +1,12 @@
 const { Response } = require('./helpers');
 var AWS = require("aws-sdk");
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-
+const messageGroupId = Date.now().toString();
 exports.handler = async(event) => {
   const params = {
     MessageBody: JSON.stringify(event),
     QueueUrl: 'https://sqs.us-west-2.amazonaws.com/209462133621/smart-dog-collar-app-test-dev-ArduinoProcessedDataQueue.fifo',
-    MessageGroupId: 'my-group-id'
+    MessageGroupId: messageGroupId
   };
 
   try {
